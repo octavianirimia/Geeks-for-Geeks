268a2b89a3f0ed3a17196da1bdbe649d3d3075f8
octavianirimia <101814478+octavianirimia@users.noreply.github.com>
2025-08-07 10:10:26 +0300

    Create recursive_approach.cpp

---
+++ Logic Building Problems/Basic Problems/Multiplication Table/C++/recursive_approach.cpp
@@ -0,0 +1,22 @@
+/*
+In this method, we pass i as an additional parameter with initial value as 1. We print n * i and then recursively call for i+1.
+We stop the recursion when i becomes 11 as we need to print only 10 multiples of given number and i.
+*/
+
+#include <iostream>
+
+void printMultiplicationTable(int number, int i) {
+    // Print the multiplication table recursively
+    if (i == 11)
+        return;
+
+    printf("%d x %d = %d\n", number, i, number * i);
+
+    printMultiplicationTable(number, ++i);
+}
+
+int main() {
+    int number = 5;
+
+    printMultiplicationTable(number, 1);
+}
