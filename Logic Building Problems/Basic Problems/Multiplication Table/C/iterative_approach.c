d88c97ef1c27b3e8c976ae71d3879234e82e5e74
octavianirimia <101814478+octavianirimia@users.noreply.github.com>
2025-08-07 10:11:00 +0300

    Create iterative_approach.c

---
+++ Logic Building Problems/Basic Problems/Multiplication Table/C/iterative_approach.c
@@ -0,0 +1,20 @@
+/*
+The iterative approach for printing a multiplication table involves using a loop to calculate and print the product of a given number and the numbers in range from 1 to 10.
+In this method, you begin with the number whose table you want to print and use a loop to multiply it with increasing values.
+*/
+
+#include <stdio.h>
+
+void printMultiplicationTable(int number) {
+    // Print the multiplication table
+
+    for (int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", number, i, number * i);
+}
+
+int main() {
+    int number = 5;
+    printMultiplicationTable(number);
+
+    return 0;
+}
