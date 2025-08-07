be1c31b5863e8493922f26ec8444e0b3af357c1f
octavianirimia <101814478+octavianirimia@users.noreply.github.com>
2025-08-07 10:41:06 +0300

    Create naive_approach.cpp

---
+++ Logic Building Problems/Basic Problems/Sum of Squares of Naturals/C++/naive_approach.cpp
@@ -0,0 +1,26 @@
+/*
+Naive Approach - Adding One By One - O(n) Time and O(1) Space
+The idea for this naive approach is to run a loop from 1 to n and sum up all the squares.
+*/
+
+#include <iostream>
+
+int sum_of_squares(int number) {
+    // Return the sum of the squared numbers from 1 to n
+
+    int sum_of_squares = 0;
+    
+    for (int i = 0; i <= number; i++)
+        sum_of_squares += i * i;
+
+    return sum_of_squares;
+}
+
+
+int main() {
+    int number = 3;
+
+    printf("%d", sum_of_squares(number));
+
+    return 0;
+}
